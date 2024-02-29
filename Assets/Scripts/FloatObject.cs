b54a3c1b038af7c88a63ba59b32b2300eec2c27f
FusTaFah <mfadel_1990@hotmail.com>
2024-02-29 23:46:17 +0000

    adds dependency floatObject for previous commit -_-. floatobject allows the creation of standalone plug in numbers

---
+++ Assets/Scripts/FloatObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "My Assets/FloatObject")]
+public class FloatObject : ScriptableObject
+{
+    public float value;
+}
